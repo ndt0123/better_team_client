acf64285546c3632bdb07763b4fc4990b707b751
tamnd-1034 <nguyen.duy.tam@sun-asterisk.com>
2021-02-16 00:00:49 +0700

    Authenticate improve

--- src/components/sign_in/sign_in.js
+++ src/components/sign_in/sign_in.js
@@ -2,10 +2,52 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {  faFacebookF , faGoogle } from '@fortawesome/free-brands-svg-icons';
+import axios from 'axios';
+import { withRouter } from 'react-router-dom';
 
 import '../../styles/sign_in.scss'
+import * as myConstant from "../../constant.js";
 
 class SignIn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+
+  handleChangeEmail = event => {
+    this.setState({ email: event.target.value });
+  }
+
+  handleChangePassword = event => {
+    this.setState({ password: event.target.value });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    axios.post(myConstant.HOST + "api/v1/sign_in", {
+      user: {
+        email: this.state.email,
+        password: this.state.password
+      }
+    }, {
+      headers: {
+        "x-rapidapi-host": "fairestdb.p.rapidapi.com",
+        "x-rapidapi-key": "apikey",
+        "content-type": "application/json",
+        "accept": "application/json"
+      }
+    })
+    .then(response => {
+      localStorage.setItem ('authentication_token', response.data.data.user.authentication_token);
+      console.log(localStorage.getItem('authentication_token'));
+      this.props.history.push('/dashboard');
+    })
+    .catch(error => console.log(error))
+  }
+
   render() {
     return(
       <div className="container">
@@ -14,30 +56,29 @@ class SignIn extends Component {
             <div className="card card-signin my-5">
               <div className="card-body">
                 <h5 className="card-title text-center">Sign In</h5>
-                <form className="form-signin">
-                  <div className="form-label-group">
-                    <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required autoFocus />
-                    <label htmlFor="inputEmail">Email address</label>
+                <form className="form-signin" onSubmit={this.handleSubmit}>
+                  <div className="form-group">
+                    <label htmlFor="exampleInputEmail1">Email address</label>
+                    <input className="email form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" type="email" required autoFocus onChange={this.handleChangeEmail} />
+                    <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
                   </div>
-
-                  <div className="form-label-group">
-                    <input type="password" id="inputPassword" className="form-control" placeholder="Password" required />
-                    <label htmlFor="inputPassword">Password</label>
+                  <div className="form-group">
+                    <label htmlFor="exampleInputPassword1">Password</label>
+                    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" required onChange={this.handleChangePassword} />
                   </div>
-
-                  <div className="custom-control custom-checkbox mb-3">
-                    <input type="checkbox" className="custom-control-input" id="customCheck1" />
-                    <label className="custom-control-label" htmlFor="customCheck1">Remember password</label>
+                  <div className="form-group form-check">
+                    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+                    <label className="form-check-label" htmlFor="exampleCheck1">Remember me</label>
                   </div>
-                  <button className="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Sign in</button>
+                  <button type="submit" className="btn btn-lg btn-primary btn-block text-uppercase">Sign in</button>
                   <div className="sign-up-div">
                     <Link to = "/sign_up">Sign up</Link>
                   </div>
                   <hr className="my-4" />
-                  <button className="btn btn-lg btn-google btn-block text-uppercase" type="submit">
+                  <button className="btn btn-lg btn-google btn-block text-uppercase">
                     <FontAwesomeIcon icon={faGoogle} className="login-icon-btn" />  Sign in with Google
                   </button>
-                  <button className="btn btn-lg btn-facebook btn-block text-uppercase" type="submit">
+                  <button className="btn btn-lg btn-facebook btn-block text-uppercase">
                     <FontAwesomeIcon icon={faFacebookF} className="login-icon-btn" />  Sign in with Facebook
                   </button>
                 </form>
@@ -50,9 +91,4 @@ class SignIn extends Component {
   }
 }
 
-export default SignIn;
-
-const backgroundRoot = {
-  background: "#007bff",
-  background: "linear-gradient(to right, #0062E6, #33AEFF)"
-}
+export default withRouter(SignIn);
--- src/components/sign_up/sign_up.js
+++ src/components/sign_up/sign_up.js
@@ -1,11 +1,70 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {  faFacebookF , faGoogle } from '@fortawesome/free-brands-svg-icons';
+import axios from 'axios';
+import { withRouter } from 'react-router-dom';
 
 import '../../styles/sign_in.scss';
+import * as myConstant from "../../constant.js";
+
 
 class SignUp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+      passwordConfirmation: ""
+    };
+  }
+
+  handleChangeFirstName = event => {
+    this.setState({ firstName: event.target.value });
+  }
+
+  handleChangeLastName = event => {
+    this.setState({ lastName: event.target.value });
+  }
+
+  handleChangeEmail = event => {
+    this.setState({ email: event.target.value });
+  }
+
+  handleChangePassword = event => {
+    this.setState({ password: event.target.value });
+  }
+
+  handleChangePasswordConfirmation = event => {
+    this.setState({ passwordConfirmation: event.target.value });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    axios.post(myConstant.HOST + "api/v1/sign_up", {
+      user: {
+        first_name: this.state.firstName,
+        last_name: this.state.lastName,
+        email: this.state.email,
+        password: this.state.password,
+        password_confirmation: this.state.passwordConfirmation
+      }
+    }, {
+      headers: {
+        "x-rapidapi-host": "fairestdb.p.rapidapi.com",
+        "x-rapidapi-key": "apikey",
+        "content-type": "application/json",
+        "accept": "application/json"
+      }
+    })
+    .then(response => {
+      localStorage.setItem ('authentication_token', response.data.data.user.authentication_token);
+      console.log(localStorage.getItem('authentication_token'));
+      this.props.history.push('/dashboard');
+    })
+    .catch(error => console.log(error))
+  }
+
   render() {
     return(
       <div className="container">
@@ -14,38 +73,32 @@ class SignUp extends Component {
             <div className="card card-signin my-5">
               <div className="card-body">
                 <h5 className="card-title text-center">Sign Up</h5>
-                <form className="form-signin">
-                  <div className="form-label-group">
-                    <input type="text" id="inputName" className="form-control" placeholder="Name" required />
-                    <label htmlFor="inputName">Name</label>
+                <form className="form-signin" onSubmit={this.handleSubmit}>
+                  <div className="form-group">
+                    <label htmlFor="inputFirstName">First name</label>
+                    <input type="text" className="form-control" id="inputFirstName" placeholder="First name" value={this.state.firstName} required onChange={this.handleChangeFirstName} />
                   </div>
-
-                  <div className="form-label-group">
-                    <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required />
-                    <label htmlFor="inputEmail">Email address</label>
+                  <div className="form-group">
+                    <label htmlFor="inputLastName">Last name</label>
+                    <input type="text" className="form-control" id="inputLastName" placeholder="Last name" value={this.state.lastName} required onChange={this.handleChangeLastName} />
                   </div>
-
-                  <div className="form-label-group">
-                    <input type="password" id="inputPassword" className="form-control" placeholder="Password" required />
+                  <div className="form-group">
+                    <label htmlFor="inputEmail1">Email address</label>
+                    <input className="email form-control" id="inputEmail1" aria-describedby="emailHelp" placeholder="Enter email" type="email"  value={this.state.email} required autoFocus onChange={this.handleChangeEmail} />
+                    <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                  </div>
+                  <div className="form-group">
                     <label htmlFor="inputPassword">Password</label>
+                    <input type="password" className="form-control" id="inputPassword" placeholder="Password" value={this.state.password} required onChange={this.handleChangePassword} />
                   </div>
-
-                  <div className="form-label-group">
-                    <input type="password" id="inputPasswordConfirmation" className="form-control" placeholder="Password confirmation" required />
+                  <div className="form-group">
                     <label htmlFor="inputPasswordConfirmation">Password confirmation</label>
+                    <input type="password" className="form-control" id="inputPasswordConfirmation" placeholder="Password confirmation" value={this.state.passwordConfirmation} required onChange={this.handleChangePasswordConfirmation} />
                   </div>
-
-                  <button className="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Sign up</button>
+                  <button type="submit" className="btn btn-lg btn-primary btn-block text-uppercase">Sign up</button>
                   <div className="sign-up-div">
                     <Link to = "/sign_in">Sign in</Link>
                   </div>
-                  <hr className="my-4" />
-                  <button className="btn btn-lg btn-google btn-block text-uppercase" type="submit">
-                   <FontAwesomeIcon icon={faGoogle} className="login-icon-btn" />  Sign in with Google
-                  </button>
-                  <button className="btn btn-lg btn-facebook btn-block text-uppercase" type="submit">
-                    <FontAwesomeIcon icon={faFacebookF} className="login-icon-btn" />  Sign in with Facebook
-                  </button>
                 </form>
               </div>
             </div>
@@ -56,9 +109,4 @@ class SignUp extends Component {
   }
 }
 
-export default SignUp;
-
-const backgroundRoot = {
-  background: "#007bff",
-  background: "linear-gradient(to right, #0062E6, #33AEFF)"
-}
+export default withRouter(SignUp);
