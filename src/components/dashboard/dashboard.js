ff7e21898fad6d0fcd504e31c0c03dfd815ce9ca
tamnd-1034 <nguyen.duy.tam@sun-asterisk.com>
2021-06-03 23:39:22 +0700

    Dashboard

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCalendarCheck, faChartLine, faCircle, faComments, faEdit, faFileAlt, faListUl, faPlus, faTrashAlt, faSignInAlt, faEye } from "@fortawesome/free-solid-svg-icons";
 import ReactTooltip from "react-tooltip";
+import axios from 'axios';
+
+import {
+  HOST
+} from "../../constant.js";
 
 import '../../styles/dashboard.scss';
 import '../../styles/main.scss';
@@ -17,26 +22,126 @@ import JoinWorkspaceConfirmModal from '../modals/workspace/join_workspace_confir
 class Dashboard extends Component {
   constructor(props) {
     super(props);
+    this.countUserTasks = this.countUserTasks.bind(this);
+    this.getPercentTaskChanged = this.getPercentTaskChanged.bind(this);
+    this.getRecentTask = this.getRecentTask.bind(this);
+    this.getRecentMessages = this.getRecentMessages.bind(this);
+
     this.state = {
       showModalTaskEditting: false,
       showModalTaskComment: false,
       showModalDeleteTask: false,
       showNewWorkspaceModal: false,
       showWokspaceDetailModal: false,
-      showJoinWorkspaceConfirmModal: false
+      showJoinWorkspaceConfirmModal: false,
+      countUserTasks: {
+        totalTasks: 0,
+        pendingTasks: 0,
+        inProgressTasks: 0,
+        finishedTasks: 0
+      },
+      percentTaskChanged: {
+        totalPercent: 0,
+        pendingPercent: 0,
+        inProgressPercent: 0,
+        finishedPercent: 0
+      },
+      recentTasks: [],
+      recentMessages: []
     }
+  }
+
+  componentDidMount() {
+    this.countUserTasks();
+    this.getPercentTaskChanged();
+    this.getRecentTask();
+    this.getRecentMessages();
+  }
+
+  countUserTasks() {
+    axios({
+      method: 'get',
+      url: HOST + 'api/v1/dashboard/count_task_user',
+      headers: {
+        'auth-token': localStorage.getItem('authentication_token')
+      }
+    }).then((response) => {
+      this.setState({
+        countUserTasks: {
+          totalTasks: response.data.total_task,
+          pendingTasks: response.data.pending_task,
+          inProgressTasks: response.data.in_progress_task,
+          finishedTasks: response.data.finished_task
+        }
+      })
+    }).catch((error) => {
+      console.log(error);
+    })
+  }
+
+  getPercentTaskChanged() {
+    axios({
+      method: 'get',
+      url: HOST + 'api/v1/dashboard/caculate_percent_task_changed',
+      headers: {
+        'auth-token': localStorage.getItem('authentication_token')
+      }
+    }).then((response) => {
+      this.setState({
+        percentTaskChanged: {
+          totalPercent: response.data.total_percent,
+          pendingPercent: response.data.pending_percent,
+          inProgressPercent: response.data.in_progress_percent,
+          finishedPercent: response.data.finished_percent
+        }
+      })
+    }).catch((error) => {
+      console.log(error);
+    })
+  }
+
+  getRecentTask() {
+    axios({
+      method: 'get',
+      url: HOST + 'api/v1/dashboard/get_recent_tasks',
+      headers: {
+        'auth-token': localStorage.getItem('authentication_token')
+      }
+    }).then((response) => {
+      this.setState({
+        recentTasks: response.data.recent_tasks
+      })
+    }).catch((error) => {
+      console.log(error);
+    })
+  }
+
+  getRecentMessages() {
+    axios({
+      method: 'get',
+      url: HOST + 'api/v1/dashboard/get_recent_messages',
+      headers: {
+        'auth-token': localStorage.getItem('authentication_token')
+      }
+    }).then((response) => {
+      this.setState({
+        recentMessages: response.data.recent_messages
+      })
+    }).catch((error) => {
+      console.log(error);
+    })
   }
 
   render() {
     return(
       <div className="main-body">
-        <div className="row">
+        <div className="row dashboard">
           <div className="col-12 col-md-6 col-lg-3 pad-b-15p">
             <div className="white-back-color bor-radius-5p shadow-box-div">
               <div className="container-fluid">
                 <div className="header-task row align-items-center">
                   <div className="col-8">
-                    <h4 className="total-tasks-value f-w-600">4000</h4>
+                    <h4 className="total-tasks-value f-w-600">{this.state.countUserTasks.totalTasks}</h4>
                     <h6 className="text-gray">Total tasks</h6>
                   </div>
                   <div className="col-4 text-right">
@@ -45,7 +150,9 @@ class Dashboard extends Component {
                 </div>
                 <div className="footer-task total-tasks-footer bor-radius-0p-0p-0p-5p row align-items-center">
                   <div className="col-8">
-                    <span className="text-white text-bold">% change</span>
+                    <span className="text-white text-bold">
+                      {this.state.percentTaskChanged.totalPercent}% change
+                    </span>
                   </div>
                   <div className="col-4 text-right text-white">
                     <FontAwesomeIcon icon={faChartLine} />
@@ -59,7 +166,7 @@ class Dashboard extends Component {
               <div className="container-fluid">
                 <div className="header-task row align-items-center">
                   <div className="col-8">
-                    <h4 className="new-tasks-value f-w-600">4000</h4>
+                    <h4 className="new-tasks-value f-w-600">{this.state.countUserTasks.pendingTasks}</h4>
                     <h6 className="text-gray">New Tasks</h6>
                   </div>
                   <div className="col-4 text-right">
@@ -68,7 +175,9 @@ class Dashboard extends Component {
                 </div>
                 <div className="footer-task new-tasks-footer bor-radius-0p-0p-0p-5p row align-items-center">
                   <div className="col-8">
-                    <span className="text-white text-bold">% change</span>
+                    <span className="text-white text-bold">
+                      {this.state.percentTaskChanged.pendingPercent}% change
+                    </span>
                   </div>
                   <div className="col-4 text-right text-white">
                     <FontAwesomeIcon icon={faChartLine} />
@@ -82,7 +191,7 @@ class Dashboard extends Component {
               <div className="container-fluid">
                 <div className="header-task row align-items-center">
                   <div className="col-8">
-                    <h4 className="inprogress-tasks-value f-w-600">4000</h4>
+                    <h4 className="inprogress-tasks-value f-w-600">{this.state.countUserTasks.inProgressTasks}</h4>
                     <h6 className="text-gray">In Progress Tasks</h6>
                   </div>
                   <div className="col-4 text-right">
@@ -91,7 +200,9 @@ class Dashboard extends Component {
                 </div>
                 <div className="footer-task inprogress-tasks-footer bor-radius-0p-0p-0p-5p row align-items-center">
                   <div className="col-8">
-                    <span className="text-white text-bold">% change</span>
+                    <span className="text-white text-bold">
+                      {this.state.percentTaskChanged.inProgressPercent}% change
+                    </span>
                   </div>
                   <div className="col-4 text-right text-white">
                     <FontAwesomeIcon icon={faChartLine} />
@@ -105,7 +216,7 @@ class Dashboard extends Component {
               <div className="container-fluid">
                 <div className="header-task row align-items-center">
                   <div className="col-8">
-                    <h4 className="completed-tasks-value f-w-600">4000</h4>
+                    <h4 className="completed-tasks-value f-w-600">{this.state.countUserTasks.finishedTasks}</h4>
                     <h6 className="text-gray">Task Completed</h6>
                   </div>
                   <div className="col-4 text-right">
@@ -114,7 +225,9 @@ class Dashboard extends Component {
                 </div>
                 <div className="footer-task completed-tasks-footer bor-radius-0p-0p-0p-5p row align-items-center">
                   <div className="col-8">
-                    <span className="text-white text-bold">% change</span>
+                    <span className="text-white text-bold">
+                      {this.state.percentTaskChanged.finishedPercent}% change
+                    </span>
                   </div>
                   <div className="col-4 text-right text-white">
                     <FontAwesomeIcon icon={faChartLine} />
@@ -141,36 +254,40 @@ class Dashboard extends Component {
                     <th scope="col">Status</th>
                     <th scope="col">Percentage</th>
                     <th scope="col">Due date</th>
-                    <th scope="col">Action</th>
+                    {/* <th scope="col">Action</th> */}
                   </tr>
                 </thead>
                 <tbody>
-                  <tr>
-                    <td className="pad-l-15p">Task number 1</td>
-                    <td className="text-gray">Your own workspace</td>
-                    <td className="text-gray">Inprogress</td>
-                    <td className="text-gray">50%</td>
-                    <td className="text-gray">23/01/2022</td>
-                    <td className="text-gray action">
-                      <div>
-                        <FontAwesomeIcon data-tip data-for="editTaskTip" className="pad-l-3p mar-r-3p mar-l-3p" icon={faEdit} onClick={() => {
-                          this.setState({
-                            showModalTaskEditting: true
-                          })
-                        }}/>
-                        <FontAwesomeIcon data-tip data-for="commentTaskTip" className="pad-l-3p mar-r-3p mar-l-3p" icon={faComments}onClick={() => {
-                          this.setState({
-                            showModalTaskComment: true
-                          })
-                        }}/>
-                        <FontAwesomeIcon data-tip data-for="deleteTaskTip" className="pad-l-3p mar-r-3p mar-l-3p" icon={faTrashAlt} onClick={() => {
-                          this.setState({
-                            showModalDeleteTask: true
-                          })
-                        }}/>
-                      </div>
-                    </td>
-                  </tr>
+                  {
+                    this.state.recentTasks.map((task, index) => (
+                      <tr key={index}>
+                        <td className="pad-l-15p">{task.title}</td>
+                        <td className="text-gray">{task.workspace_title}</td>
+                        <td className="text-gray">{task.status}</td>
+                        <td className="text-gray">{task.percentage_completed}%</td>
+                        <td className="text-gray">{task.due_date}</td>
+                        {/* <td className="text-gray action">
+                          <div>
+                            <FontAwesomeIcon data-tip data-for="editTaskTip" className="pad-l-3p mar-r-3p mar-l-3p" icon={faEdit} onClick={() => {
+                              this.setState({
+                                showModalTaskEditting: true
+                              })
+                            }}/>
+                            <FontAwesomeIcon data-tip data-for="commentTaskTip" className="pad-l-3p mar-r-3p mar-l-3p" icon={faComments}onClick={() => {
+                              this.setState({
+                                showModalTaskComment: true
+                              })
+                            }}/>
+                            <FontAwesomeIcon data-tip data-for="deleteTaskTip" className="pad-l-3p mar-r-3p mar-l-3p" icon={faTrashAlt} onClick={() => {
+                              this.setState({
+                                showModalDeleteTask: true
+                              })
+                            }}/>
+                          </div>
+                        </td> */}
+                      </tr>
+                    ))
+                  }
                 </tbody>
               </table>
               </div>
@@ -185,22 +302,26 @@ class Dashboard extends Component {
                 <div className="col-6"></div>
               </div>
               <div className="">
-                <div className="col-12 mar-0p pad-t-10p pad-b-10p hover-gray-back row align-items-center">
-                  <div className="col-10 pad-0p">
-                    <div className="col-12 pad-l-0p pad-r-0p">Message 1</div>
-                    <div className="col-12 pad-l-0p pad-r-0p text-small text-gray">
-                      Nguyen Duy Tam: Content message 1
+                {
+                  this.state.recentMessages.map((message, index) => (
+                    <div className="col-12 mar-0p pad-t-10p pad-b-10p hover-gray-back row align-items-center">
+                      <div className="col-10 pad-0p">
+                        <div className="col-12 pad-l-0p pad-r-0p">{message.workspace_title}</div>
+                        <div className="col-12 pad-l-0p pad-r-0p text-small text-gray">
+                          <span className="dashboard-user-name-message">{message.user_name}</span>: {message.content}
+                        </div>
+                      </div>
+                      <div className="col-2 pad-0p">
+                        <FontAwesomeIcon icon={faCircle} className="fa-xs fa-pull-right text-green" />
+                      </div>
                     </div>
-                  </div>
-                  <div className="col-2 pad-0p">
-                    <FontAwesomeIcon icon={faCircle} className="fa-xs fa-pull-right text-green" />
-                  </div>
-                </div>
+                  ))
+                }
               </div>
             </div>
           </div>
 
-          <div className="col-md-12 col-lg-4 pad-t-15p pad-b-15p">
+          {/* <div className="col-md-12 col-lg-4 pad-t-15p pad-b-15p">
             <div className="white-back-color bor-radius-5p shadow-box-div pad-b-15p pad-t-15p">
               <div className="box-dash-title-container row pad-b-10p">
                 <div className="col-6">
@@ -271,7 +392,7 @@ class Dashboard extends Component {
               </table>
               </div>
             </div>
-          </div>
+          </div> */}
         </div>
 
 
